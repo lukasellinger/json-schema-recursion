715c3f2873b8d4ecede8b6b670542534cb366abc
Lukas Ellinger <lukasellinger@macbook-air.fritz.box>
2021-09-23 17:59:57 +0200

    asdf

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -31,48 +31,51 @@ public class Main {
    *        directory in which the normalized schemas are. These will be checked for recursion. If
    *        -stats is chosen, second parameter will be the path to the directory with unnormalized
    *        schemas and third parameter the path to the directory with normalized schemas.
-   * 
    * @throws IOException
    */
   public static void main(String[] args) throws IOException {
-    switch (args[0]) {
-      case "-normalize":
-        boolean allowDistributedSchemas = Boolean.parseBoolean(args[2].substring(1));
-        boolean fetchSchemasOnline = Boolean.parseBoolean(args[3].substring(1));
-        LoadSchemaDTO config = new LoadSchemaDTO.Builder()
-            .allowDistributedSchemas(allowDistributedSchemas)
-            .fetchSchemasOnline(fetchSchemasOnline)
-            .build();
-        switch (args[1]) {
-          case "-corpus":
-            SchemaCorpus corpus = new SchemaCorpus();
-            config.setRepType(RepositoryType.CORPUS);
-            corpus.normalize(new File(args[4]), new File(args[5]), config);
-            break;
-          case "-testsuite":
-            TestSuite suite = new TestSuite();
-            config.setRepType(RepositoryType.TESTSUITE);
-            suite.normalize(new File(args[4]), config);
-            break;
-          case "-normal":
-            DirNormalizer normalizer = new DirNormalizer();
-            config.setRepType(RepositoryType.NORMAL);
-            normalizer.normalize(new File(args[4]), config);
-            break;
-          default:
-            throw new IllegalArgumentException("Unexpected value: " + args[1]);
-        }
-        break;
-      case "-recursion":
-        Analyser analyser1 = new Analyser();
-        analyser1.analyseRecursion(new File(args[1]));
-        break;
-      case "-stats":
-        Analyser analyser2 = new Analyser();
-        analyser2.createDetailedStats(new File(args[1]), new File(args[2]));
-        break;
-      default:
-        throw new IllegalArgumentException("Unexpected value: " + args[0]);
+    if (args.length < 2) {
+      throw new IllegalArgumentException("To less parameters");
+    } else {
+      switch (args[0]) {
+        case "-normalize":
+          boolean allowDistributedSchemas = Boolean.parseBoolean(args[2].substring(1));
+          boolean fetchSchemasOnline = Boolean.parseBoolean(args[3].substring(1));
+          LoadSchemaDTO config = new LoadSchemaDTO.Builder()
+              .allowDistributedSchemas(allowDistributedSchemas)
+              .fetchSchemasOnline(fetchSchemasOnline)
+              .build();
+          switch (args[1]) {
+            case "-corpus":
+              SchemaCorpus corpus = new SchemaCorpus();
+              config.setRepType(RepositoryType.CORPUS);
+              corpus.normalize(new File(args[4]), new File(args[5]), config);
+              break;
+            case "-testsuite":
+              TestSuite suite = new TestSuite();
+              config.setRepType(RepositoryType.TESTSUITE);
+              suite.normalize(new File(args[4]), config);
+              break;
+            case "-normal":
+              DirNormalizer normalizer = new DirNormalizer();
+              config.setRepType(RepositoryType.NORMAL);
+              normalizer.normalize(new File(args[4]), config);
+              break;
+            default:
+              throw new IllegalArgumentException("Unexpected value: " + args[1]);
+          }
+          break;
+        case "-recursion":
+          Analyser analyser1 = new Analyser();
+          analyser1.analyseRecursion(new File(args[1]));
+          break;
+        case "-stats":
+          Analyser analyser2 = new Analyser();
+          analyser2.createDetailedStats(new File(args[1]), new File(args[2]));
+          break;
+        default:
+          throw new IllegalArgumentException("Unexpected value: " + args[0]);
+      } 
     }
   }
 }
